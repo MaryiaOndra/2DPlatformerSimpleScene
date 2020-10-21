e0b8fcbc325aa6125d1d990c419807ac984cdbc8
Maryia Ondra <maryiaondra@gmail.com>
2020-10-21 18:14:33 +0300

    added animation to  zombie

--- 2DSimpleLevel/Assets/Scripts/EnemyMovement.cs
+++ 2DSimpleLevel/Assets/Scripts/EnemyMovement.cs
@@ -8,32 +8,51 @@ public class EnemyMovement : MonoBehaviour
     private int currentPoint;
     private Transform[] points;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         points = new Transform[path.childCount];
-        animator = GetComponent<Animator>();
 
         for (int i = 0; i < path.childCount; i++)
         {
             points[i] = path.GetChild(i);
         }
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        //make object move from one point to another
         Transform target = points[currentPoint];
-
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
         if (transform.position == target.position)
         {
             currentPoint++;
+            spriteRenderer.flipX = false;
 
             if (currentPoint >= points.Length)
             {
                 currentPoint = 0;
+                spriteRenderer.flipX = true;
             }
         }
+
+        animator.SetFloat("Speed", speed);
     }
+
+    //private void FlipPlayerSprite()
+    //{
+    //    if (xValue < 0)
+    //    {
+    //        spriteRenderer.flipX = true;
+    //    }
+    //    else if (xValue > 0)
+    //    {
+    //        spriteRenderer.flipX = false;
+    //    }
+    //}
 }
