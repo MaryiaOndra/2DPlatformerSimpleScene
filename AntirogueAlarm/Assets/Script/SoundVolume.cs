70a28d2ec207fd2d24ca5e0221ac54b13f998b18
Maryia Ondra <maryiaondra@gmail.com>
2020-10-24 23:27:16 +0300

    tried to fi audioSourse

--- AntirogueAlarm/Assets/Script/SoundVolume.cs
+++ AntirogueAlarm/Assets/Script/SoundVolume.cs
@@ -2,14 +2,16 @@
 
 public class SoundVolume : MonoBehaviour
 {
-    private float minVolume = 0.3f;
+    private float minVolume = 0.2f;
     private float maxVolume = 1f;
     private float targetVolume;
-    private float changeMargin = 0.2f;
+    private float changeMargin = 0.1f;
+
+    [SerializeField] private float fadeSpeed;
 
     private AudioSource audioSource;
 
-    internal bool soundOn;
+    [SerializeField] internal bool soundOn;
 
 
     // Start is called before the first frame update
@@ -23,13 +25,14 @@ public class SoundVolume : MonoBehaviour
     void Update()
     {
         if (soundOn)
-        {            
-            audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, Time.deltaTime);
+        {
+            Debug.Log("soundOn");
+            audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, Time.deltaTime * fadeSpeed);
             CheckSoundVolume();
         }
         else
         {
-            audioSource.volume = Mathf.Lerp(audioSource.volume, 0f, Time.deltaTime);
+            audioSource.volume = Mathf.Lerp(audioSource.volume, 0f, Time.deltaTime * fadeSpeed);
         }
     }
 
