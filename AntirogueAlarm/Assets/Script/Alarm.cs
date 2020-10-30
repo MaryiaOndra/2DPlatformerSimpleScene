f0f54e1412c32df8d334faeb34c63a919dd3a164
Maryia Ondra <maryiaondra@gmail.com>
2020-10-30 16:45:33 +0300

    fixed script Alarm

--- AntirogueAlarm/Assets/Script/Alarm.cs
+++ AntirogueAlarm/Assets/Script/Alarm.cs
@@ -1,13 +1,9 @@
 ﻿using UnityEngine;
 
-
 [RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Light))]
 
 public class Alarm : MonoBehaviour
 {
-    private void Start()
-    {
-        gameObject.SetActive(false);
-    }
+
 }
